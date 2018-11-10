b560c99ceb73b31cc380b5aed2eee22c3ed89973
Christian Camacho <Christian Camacho>
2018-11-09 17:50:40 -0800

    Remove api and test motor

--- dispenser/main.py
+++ dispenser/main.py
@@ -1,4 +1,19 @@
-def print_kwarg(**kwargs):
-    print(kwargs.get(kwargs.get('key')))
+from nanpy import ArduinoApi, SerialManager, Stepper
 
-print_kwarg(name="christian", key="k")
+serial = SerialManager(device='/dev/ttyACM0')
+
+motor = Stepper(
+    revsteps=3800,
+    pin1=1,
+    pin2=2,
+    pin3=3,
+    pin4=4,
+    connection=serial,
+    speed=10
+)
+
+x = 100
+
+while True:
+    x = x + 100
+    motor.step(x)
